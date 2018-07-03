05b31130853d93b4a00dc091ece99adebd12a193
John Franey <johnfraney@gmail.com>
2018-07-02 21:48:54 -0300

    Add Flake8 and fix linting errors

--- ner_example/urls.py
+++ ner_example/urls.py
@@ -23,9 +23,17 @@ urlpatterns = [
     path('entities/create/', views.EntityCreate.as_view(), name='entity-create'),
     path('phrases/', views.PhraseList.as_view(), name='phrase-list'),
     path('phrases/create/', views.PhraseCreate.as_view(), name='phrase-create'),
-    path('phrases/untagged/', views.PhraseListUntagged.as_view(), name='phrase-list-untagged'),
+    path(
+        'phrases/untagged/',
+        views.PhraseListUntagged.as_view(),
+        name='phrase-list-untagged'
+    ),
     path('phrases/<int:pk>/', views.PhraseDetail.as_view(), name='phrase-detail'),
-    path('phrases/<int:pk>/set-entities/', views.set_phrase_entities, name='phrase-set-entities'),
+    path(
+        'phrases/<int:pk>/set-entities/',
+        views.set_phrase_entities,
+        name='phrase-set-entities'
+    ),
     path('api/', include(ner_trainer_urls)),
     path('admin/', admin.site.urls),
 ]
